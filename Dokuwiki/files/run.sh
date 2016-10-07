0bd428058ad793a4e3338e48419207c270d41a68
André <andre.raabe@gmail.com>
2016-10-07 17:40:56 +0200

    Add some plugins for Dokuwiki

--- Dokuwiki/files/run.sh
+++ Dokuwiki/files/run.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-chown -R nginx:nginx /usr/share/nginx/html
+chown -R nginx:nginx /var/lib/nginx/html
 php-fpm -D -c /etc/php5/php.ini -y /etc/php5/php-fpm.conf -d variables_order="EGPCS"
 nginx -g "daemon off;"
 
