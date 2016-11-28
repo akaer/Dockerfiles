18a81f7979f4bc0fd3437233e5029a1538897db1
André <andre.raabe@gmail.com>
2016-11-28 18:44:17 +0100

    Add generatedata

---
+++ generatedata/web/files/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbHostname     = '@@MYSQL_DB_HOSTNAME@@';
+$dbName         = '@@MYSQL_DB_NAME@@';
+$dbUsername     = '@@MYSQL_DB_USER@@';
+$dbPassword     = '@@MYSQL_DB_PASSWORD@@';
+$dbTablePrefix  = 'gd_';
+$encryptionSalt = 'f0r';
+
+?>
