50cf9fdb1faa44ed9813f354473b9844c252c9a3
André <andre.raabe@gmail.com>
2016-11-29 17:02:13 +0100

    Improve dokuwiki

---
+++ Dokuwiki/files/doku-conf-local.php
@@ -0,0 +1,10 @@
+<?php
+$conf['title'] = '@@WIKINAME@@';
+$conf['lang'] = 'en';
+$conf['license'] = '0';
+$conf['useacl'] = 0;
+$conf['superuser'] = '@admin';
+$conf['disableactions'] = 'register';
+$conf['start'] = 'doc:readme';
+$conf['userewrite'] = 0;
+
--- Dokuwiki/files/run.sh
+++ Dokuwiki/files/run.sh
@@ -2,7 +2,18 @@
 
 set -ex
 
-chown -R nginx:nginx /var/lib/nginx/html
+if [[ ! -e /var/lib/nginx/html/dokuwiki/data/pages/doc/readme.txt ]]; then
+    echo "====== Welcome to your wiki ======" > \
+        /var/lib/nginx/html/dokuwiki/data/pages/doc/readme.txt
+fi
+
+sed -i "s|@@WIKINAME@@|${WIKINAME}|g" /var/lib/nginx/html/dokuwiki/conf/local.php
+
+chown nginx:nginx -R /var/lib/nginx/html/dokuwiki/data
+chown nginx:nginx -R /var/lib/nginx/html/dokuwiki/conf
+chown nginx:nginx -R /var/lib/nginx/html/dokuwiki/lib/plugins
+chown nginx:nginx -R /var/lib/nginx/tmp/
+
 php-fpm -D -c /etc/php5/php.ini -y /etc/php5/php-fpm.conf -d variables_order="EGPCS"
 nginx -g "daemon off;"
 
