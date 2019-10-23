290d5c092abd1d6d450ffa96d7a6d2b4bbc60827
André <andre.raabe@gmail.com>
2019-10-23 10:10:52 +0200

    WIP

--- sql-server-2017-linux-multi/db/init_server.sh
+++ sql-server-2017-linux-multi/db/init_server.sh
@@ -12,11 +12,6 @@ echo "[*] Pre customizing sql server"
 cp -iv /tmp/mssql.conf /var/opt/mssql/mssql.conf
 replace_vars /var/opt/mssql/mssql.conf
 
-openssl req -x509 -nodes -newkey rsa:4096 \
-    -subj '/CN='${HOSTNAME} \
-    -keyout "/ssl/${HOSTNAME}_mssql.key" \
-    -out "/ssl/${HOSTNAME}_mssql.pem" -days 365
-
 echo "[*] Pre customizing sql server done"
 
 exit 0
