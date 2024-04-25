6e839d56915d73473caf3dd962b563bcc04eb08f
André Raabe <andre.raabe@gmail.com>
2024-04-25 12:23:20 +0200

    Fix

--- Selenium-Chrome-Powershell/samples/demo01.tests.ps1
+++ Selenium-Chrome-Powershell/samples/demo01.tests.ps1
@@ -5,13 +5,17 @@ Import-Module Selenium
 Import-Module Pester
 
 $Global:testURL = 'https://google.de'
+$Global:Width = 1360
+$Global:Height = 1020
 
-Describe "All in one Test" {
-
+Describe 'Simple Google search' {
     BeforeAll {
         $chromeOptions = New-Object OpenQA.Selenium.Chrome.ChromeOptions
+
+        # https://peter.sh/experiments/chromium-command-line-switches/
+        $chromeOptions.AddArguments('headless', 'disable-gpu', 'no-sandbox', 'enable-logging', 'no-default-browser-check', 'no-first-run', 'remote-debugging-address=0.0.0.0', 'remote-debugging-port=9222')
         $driver = New-Object OpenQA.Selenium.Chrome.ChromeDriver($chromeOptions)
-        $driver.Manage().Window.Maximize()
+        $driver.Manage().Window.Size = new-Object System.Drawing.Size($Global:Width, $Global:Height)
         $driver.Navigate().GoToUrl($Global:testURL)
     }
 
@@ -19,34 +23,34 @@ Describe "All in one Test" {
         $driver.Title | Should -Be 'Google'
     }
 
-    Describe "Can handle cookie banner" {
+    Describe 'Cookie banner handling' {
 
         BeforeAll {
             $cookieBtn = $driver.FindElementByXPath('//*[@id="L2AGLb"]/div')
         }
 
-        It "Can find the cookie warning" {
+        It 'Can find the cookie warning' {
             $cookieBtn | Should -Not -BeNullOrEmpty
             $cookieBtn.Text | Should -Be 'Alle akzeptieren'
         }
 
-        It "Can accept the cookie warning" {
+        It 'Can accept the cookie warning' {
             $cookieBtn.Click()
         }
     }
 
-    Describe "Can perform a search" {
+    Describe 'Perform a search' {
 
         BeforeAll {
-            $search = $driver.FindElementByName("q")
+            $search = $driver.FindElementByName('q')
         }
 
-        It "Can find the search input" {
+        It 'Can find the search input' {
             $search | Should -Not -BeNullOrEmpty
         }
 
-        It "Can enter search term and submit" {
-            $search.SendKeys("Selenium Pester Powershell")
+        It 'Can enter search term and submit' {
+            $search.SendKeys('Selenium Pester Powershell')
             $search.Submit()
         }
     }
@@ -56,3 +60,4 @@ Describe "All in one Test" {
         $driver.Quit()
     }
 }
+
